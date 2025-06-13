6579a3820990066be1936d51d7efcb3010d1ffc4
junkim0 <chrisandquartz@gmail.com>
2025-06-13 17:02:37 +0900

    refactor: restore /api/friend route and remove blocking configs

--- next.config.mjs
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // This file is added to force Vercel to re-evaluate the project structure
-  // and hopefully clear the stuck deployment cache.
-};
-
-export default nextConfig; 
